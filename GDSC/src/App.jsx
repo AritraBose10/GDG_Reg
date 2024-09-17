a9a0a10cd89d7293bce95d582ce35a8017d1baf7
Aritra Bose <93369671+AritraBose10@users.noreply.github.com>
2024-09-17 19:11:21 +0530

    Update App.jsx

--- GDSC/src/App.jsx
+++ GDSC/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<GDGRegistrationForm />} />
-        <Route path="/success" element={<SuccessPage />} />
+       
       </Routes>
     </Router>
   );
