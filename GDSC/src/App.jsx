d49a413cb3a1088e2cc1a15607916a5246b1a615
Aritra Bose <93369671+AritraBose10@users.noreply.github.com>
2024-09-17 19:11:30 +0530

    Update App.jsx

--- GDSC/src/App.jsx
+++ GDSC/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import GDGRegistrationForm from "./regis";
-import SuccessPage from "./success";
+
 
 function App() {
   return (
